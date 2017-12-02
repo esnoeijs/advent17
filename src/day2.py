7ef5ead02b0dd04063dcbd907e97ab5da28e49e9
esnoeijs <eriksnoeijs@gmail.com>
2017-12-02 14:36:27 +0100

    day2 part2 done, could probably use a bunch of improvement but wanted to do it as a one-liner :P

--- src/day2.py
+++ src/day2.py
@@ -1,3 +1,4 @@
+import itertools
 
 """
  5 1 9 5
@@ -8,7 +9,25 @@
  The third row's difference is 6.
 """
 def part1(text):
-    return sum(abs(max(rowInts) - min(rowInts)) for rowInts in (list(map(int, row)) for row in (line.split() for line in text.split("\n"))))
+    return sum(abs(max(rowInts) - min(rowInts)) for rowInts in
+               (list(map(int, row)) for row in
+                (line.split() for line in text.split("\n"))))
+
+"""
+5 9 2 8
+9 4 7 3
+3 8 6 5
+In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+In the second row, the two numbers are 9 and 3; the result is 3.
+In the third row, the result is 2.
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+What is the sum of each row's result in your puzzle input?
+"""
+def part2(text):
+    return int(sum(list(filter(lambda x: x.is_integer(), map(lambda pair: pair[0]/pair[1] if (pair[0]/pair[1]).is_integer() else pair[1]/pair[0], itertools.combinations(rowInts, 2))))[0] for rowInts in
+            (list(map(int, row)) for row in
+             (line.split() for line in text.split("\n")))))
 
 with open("data/day2input.txt") as file:
-    print(part1(file.read()))
+    print(part2(file.read()))

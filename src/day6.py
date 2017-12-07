1e157518988befd6b730cd1cc9023178e3e69fbc
erik.snoeijs <erik.snoeijs@bax-shop.nl>
2017-12-07 13:05:04 +0100

    day6 part2

--- src/day6.py
+++ src/day6.py
@@ -6,8 +6,8 @@ def part1(banks):
     while True:
         banks_hash = tuple(banks)
         if banks_hash in seen_list:
-            return steps
-        seen_list[banks_hash] = True
+            return (steps, steps - seen_list[banks_hash])
+        seen_list[banks_hash] = steps
         pointer, value = max(enumerate(banks), key=operator.itemgetter(1))
         banks[pointer] = 0
         steps += 1
@@ -21,8 +21,8 @@ def part1(banks):
 
 
 text = [0,2,7,0]
-print("p1: %s" % (part1(text)))
+print("p1: %s, P2: %s" % (part1(text)[0], part1(text)[1]))
 
 with open("data/day6input.txt") as file:
     text =  list(map(int, file.read().split()))
-    print("p1: %s" % (part1(text)))
+    print("p1: %s, P2: %s" % (part1(text)[0], part1(text)[1]))
